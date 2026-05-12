b9b02bc429d5b6fc87c0d556de1651656a8c8bbf
小安 <xiaoan@xiaoandeMac-mini.local>
2026-05-12 19:23:55 +0800

    feat: add AutoVolume agent entrypoint

--- AutoVolume/Sources/AutoVolumeAgent/main.swift
+++ AutoVolume/Sources/AutoVolumeAgent/main.swift
@@ -1,4 +1,29 @@
 import Foundation
+import AutoVolumeShared
 
-print("AutoVolumeAgent ready")
+let store = JSONConfigStore()
+let engine = AgentEngine(
+    mountState: FileSystemMountStateProvider(),
+    credentialStore: KeychainCredentialStore(),
+    commandRunner: ProcessCommandRunner(),
+    mountPlanner: MountPlanner()
+)
+
+func runOnce() {
+    do {
+        let configs = try store.load()
+        for config in configs where config.isEnabled {
+            _ = try engine.check(config)
+        }
+    } catch {
+        fputs("AutoVolumeAgent error: \(error)\n", stderr)
+    }
+}
+
+let timer = Timer(timeInterval: 60, repeats: true) { _ in
+    runOnce()
+}
+
+RunLoop.main.add(timer, forMode: .default)
+runOnce()
 RunLoop.main.run()
